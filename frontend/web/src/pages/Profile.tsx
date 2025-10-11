e4d97e95ca1564d1e733236ea04fee02cee0ca30
wongzhifeng <wongzhifeng@126.com>
2025-10-11 13:46:26 +0800

    🔧 修复JSX语法错误
    - 修复Profile.tsx中'>'字符的JSX转义问题
    - 将'>'改为'&gt;'以符合JSX规范
    - 解决Zeabur部署构建失败问题

--- frontend/web/src/pages/Profile.tsx
+++ frontend/web/src/pages/Profile.tsx
@@ -60,7 +60,7 @@ const Profile: React.FC = () => {
               >
                 <Icon className="w-5 h-5 mr-4 text-gray-600" />
                 <span className="flex-1 text-left font-medium">{item.label}</span>
-                <span className="text-gray-400">></span>
+                <span className="text-gray-400">&gt;</span>
               </button>
             )
           })}
